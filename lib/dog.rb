0bb1b1365940ee5eddd81045a62fbc632261cb53
Alison Reilly <reillyamr@gmail.com>
2019-02-23 00:23:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,30 +19,39 @@ class Dog
   end 
   
   def save 
-    if self.id 
-      self.update 
-    else 
+    # if self.id 
+    #   self.update 
+    # else 
       sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)" 
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    end 
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      return self
+    # end 
   end 
   
   def self.create(hash)
     @name = hash[:name]
     @breed = hash[:breed]
-    new_student = self.new(@name, @breed)
+    new_student = self.new(name: hash[:name], breed: hash[:breed] )
     new_student.save 
   end 
   
-  def self.find_by_id (id)
+  def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     DB[:conn].execute(sql, id).map do |row| self.new_from_db(row) 
     end.first 
   end 
 
-  def self.find_or_create_by 
+  def self.find_or_create_by
+    
   end 
+  
+  def self.new_from_db(row) 
+    # id = 
+    # name = 
+    # breed = 
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
   
   def self.find_by_name(name)
     
