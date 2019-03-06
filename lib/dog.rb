94b266a5927ac2a1d3f6e0d0402a272579749bba
Diana Badas <diana.badas@gmail.com>
2019-03-06 14:45:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,6 +81,18 @@ class Dog
     dog
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
 
