77ed946e5754e6d10e2f00131bbc259399474f72
J. Singleton <jennifer.singleton@gmail.com>
2019-03-17 19:16:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,23 +26,87 @@ class Dog
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO dogs (name, breed)
-      VALUES (?, ?)
-    SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
 
-    DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
     self
   end
 
   def self.create(name:, breed:)
-    dog = self.new(name, breed)
+    dog = self.new(name: name, breed:breed)
     dog.save
     dog
   end
 
-  def new_from_db
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
 
+    self.new(id:id, name:name, breed:breed)
   end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+    SQL
+
+    row = DB[:conn].execute(sql, id)[0]
+    new_dog = new_from_db(row)
+    new_dog
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+
+    row = DB[:conn].execute(sql, name)[0]
+    new_dog = new_from_db(row)
+    new_dog
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ? AND breed = ?
+    SQL
+
+    dog = DB[:conn].execute(sql, name, breed)
+
+    if !dog.empty?
+      dog_info = dog[0]
+      dog = Dog.new(id:dog_info[0], name:dog_info[1], breed:dog_info[2])
+    else
+      dog = self.create(name:name, breed:breed)
+    end
+    dog
+  end
+
+  def update
+    Dog.find_by_id(self.id)
+
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+    self
+  end
+
 end
