16538c6bb685b2287804ce3eebafe6ea206a28a6
Bry Cox <carl.cc@gmail.com>
2019-05-13 02:44:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,70 @@
 class Dog
 
-  attr_accessor :name, :breed
-  attr_writer :id
+  attr_accessor :name, :breed, :id
 
   def initialize(arg)
-    binding.pry
+        arg.each_pair {|key, value| self.send(("#{key}="), value)}
+  end
+
+  def self.create_table
+
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+
+  end
+
+  def self.drop_table
+
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+
+  end
+
+  def save
+
+   if self.id
+      self.update
+
+   else
+
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+   end
+ end
+
+ def update
+
+    sql = "UPDATE dogs SET name = ?, breed= ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+
+  end
+
+  def self.find_by_name(name)
+
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ? LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map {|data_base_row| self.new_from_db(data_base_row)}[0]
+
+  end
+
+  def self.new_from_db(row)
+    self.new(row[0], row[1], row[2])
   end
 
 
