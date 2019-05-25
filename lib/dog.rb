10b2b8c8311df20d5345e63fd8011b1dd0b16664
Yingqi Chen <chanwk18@hotmail.com>
2019-05-25 01:10:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+attr_accessor :name,:breed,:id
+
+def initialize(name:,breed:,id:nil)
+  @name=name
+  @breed=breed
+  @id=id
+ end
+
+ def self.create_table
+    sql=<<-SQL
+       CREATE TABLE dogs(
+         id INTEGER PRIMARY KEY,
+         name TEXT,
+         breed TEXT
+       );
+    SQL
+
+    DB[:conn].execute(sql)
+
+end
+
+ def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs")
+end
+
+end
