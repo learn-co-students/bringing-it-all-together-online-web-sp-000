5c4b05a2faa6bbf6ffed0e26d3d37b0def99bc1e
Yingqi Chen <chanwk18@hotmail.com>
2019-05-29 22:08:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,9 +45,36 @@ def self.find_by_id(id)
       SELECT * FROM dogs WHERE id=?
    SQL
    #DB[:conn].results_as_hash = true
-   binding.pry
+   #binding.pry
    hash=DB[:conn].execute(sql,id)[0]
-   Dog.new(hash)
+   Dog.new_from_db(hash)
+end
+
+def self.find_or_create_by(hash)
+  sql=<<-SQL
+    SELECT * FROM dogs WHERE name=? AND breed=?
+    Limit 1
+  SQL
+
+  dog=DB[:conn].execute(sql,hash[:name],hash[:breed])[0]
+  if dog
+    Dog.new_from_db(dog)
+  else
+    Dog.create(hash)
+  end
+end
+
+def self.new_from_db(row)
+  dog=Dog.new({id:row[0],name:row[1],breed:row[2]})
+end
+
+def self.find_by_name(name)
+  dog=DB[:conn].execute("SELECT * FROM dogs WHERE name=?",name)[0]
+  Dog.new_from_db(dog)
+end
+
+def update
+  DB[:conn].execute("UPDATE dogs SET name=?, breed=? WHERE id=?",self.name,self.breed,self.id)
 end
 
 end
