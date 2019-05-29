548555d92a08bf999240d38a28360350486308ab
Yingqi Chen <chanwk18@hotmail.com>
2019-05-29 19:21:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,4 +24,21 @@ end
     DB[:conn].execute("DROP TABLE dogs")
 end
 
+def save
+  sql=<<-SQL
+    INSERT INTO dogs (name,breed) VALUES (?,?)
+  SQL
+
+  DB[:conn].execute(sql,self.name,self.breed)
+  self.id=DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  self
+end
+
+
+def self.create(name:,breed:)
+
+
+
+end
+
 end
