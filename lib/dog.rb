50efe507b5308209cd2c40a5e16259e01814b7c1
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 23:20:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,11 +14,34 @@ class Dog
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     self
   end
 
-  def self.find_or_create_by(name:, breed:)
+  def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name).collect{|row| self.new(id: row[0], name: row[1], breed: row[2]) }.first
+  end
+
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      self.find_by_id(dog[0][0])
+    else
+      self.create(name: name, breed: breed)
+    end
   end
 
   def self.find_by_id(id)
