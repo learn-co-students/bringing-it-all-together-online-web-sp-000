0253fef27115b6a44c787ba4e5acffcd1c7d0b44
Alan Krajina <alankrajina88@gmail.com>
2019-06-27 12:26:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,6 +33,73 @@ class Dog
       return self
   end
   
+  def self.create(hash)
+    dog = Dog.new(hash)
+    dog.save
+    dog
+  end
   
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(id:result[0],name:result[1],breed:result[2])    
+  end
+  
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      result = dog[0]
+      dog = Dog.new(id:result[0],name:result[1],breed:result[2]) 
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end 
+  
+  def self.new_from_db(row)
+  	new_dog = self.new 
+  	new_dog.id = id:row[0]
+  	new_dog.name =  name:row[1]
+  	new_dog.breed = breed:row[2]
+  	new_dog    
+  end
   
 end
+
+=begin
+
+  describe '.new_from_db' do
+    it 'creates an instance with corresponding attribute values' do
+      row = [1, "Pat", "poodle"]
+      pat = Dog.new_from_db(row)
+
+      expect(pat.id).to eq(row[0])
+      expect(pat.name).to eq(row[1])
+      expect(pat.breed).to eq(row[2])
+    end
+  end
+
+  describe '.find_by_name' do
+    it 'returns an instance of dog that matches the name from the DB' do
+      teddy.save
+      teddy_from_db = Dog.find_by_name("Teddy")
+
+      expect(teddy_from_db.name).to eq("Teddy")
+      expect(teddy_from_db.id).to eq(1)
+      expect(teddy_from_db).to be_an_instance_of(Dog)
+    end
+  end
+
+  describe '#update' do
+    it 'updates the record associated with a given instance' do
+      teddy.save
+      teddy.name = "Teddy Jr."
+      teddy.update
+      teddy_jr = Dog.find_by_name("Teddy Jr.")
+      expect(teddy_jr.id).to eq(teddy.id)
+    end
+
+  end
+
+end
+=end
