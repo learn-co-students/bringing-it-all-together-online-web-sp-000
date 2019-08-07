f4b01653986ad62f7dd129674493fea68ab019b0
Frederic Silber <freddy.silber@cloudcoach.com>
2019-08-07 03:00:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,6 +6,33 @@ class Dog
     self.id ||= nil
   end
 
-  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY)
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute('SELECT last_insert_rowId() FROM dogs')[0][0]
+    self
+  end
+
+  def self.create(hash)
+    new = Dog.new(hash)
+    new.save
+    new
+  end
 
 end
