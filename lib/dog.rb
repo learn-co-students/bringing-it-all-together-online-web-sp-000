c4b6b4b5cc35e1bf6a5b8e9094014327ca506df9
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-02 18:58:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,6 +32,50 @@ def self.create(hash)
   dog = Dog.new(hash)
   dog.save
 end
+def self.new_from_db(row)
+  dog_hash = {
+    :id => row[0],
+    :name => row[1],
+    :breed => row[2]
+  }
+  self.new(dog_hash)
+end
+
+def self.find_by_id(id)
+  sql = <<-SQL
+  SELECT * FROM dogs WHERE id = ?
+  SQL
+  DB[:conn].execute(sql, id).map do |row|
+    self.new_from_db(row)
+  end.first
+end
+
+def self.find_or_create_by(name:, breed:)
+  sql = <<-SQL
+  SELECT * FROM dogs WHERE name = ? AND breed = ?
+  SQL
+  dog = DB[:conn].execute(sql, name, breed).first
+  if dog
+    doggy = self.new_from_db(dog)
+  else
+    doggy = self.create({:name => name, :breed => breed})
+  end
+  doggy
+end
+def self.find_by_name(name)
+  sql = <<-SQL
+  SELECT * FROM dogs WHERE name = ?
+  SQL
+  DB[:conn].execute(sql, name).map do |row|
+    dog = self.new_from_db(row)
+  end.first
+end
+def update
+  sql = <<-SQL
+  UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+  SQL
+  DB[:conn].execute(sql, self.name, self.breed, self.id)
+end
 
 
 end
