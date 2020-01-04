f67d93675b20b31af2da4a79dc41a9d818b8fefb
Anthony Tilelli <anthony@tilelli.me>
2020-01-04 04:43:16 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,42 @@
+require "pry"
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+  
+  def initialize(name:, breed:, id:nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    );
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    DB[:conn].execute('DROP TABLE IF EXISTS dogs')
+  end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO dogs( name, breed ) VALUES (?,?);"
+      DB[:conn].execute(sql, self.name, self.breed)
+    end
+    self
+  end
+  
+
+  def update
+    sql = "UPDATE dogs SET name == ?, breed == ?  WHERE id == ?"
+    DB[:conn].execute(sql, @name, @breed, @id)
+  end
+end
