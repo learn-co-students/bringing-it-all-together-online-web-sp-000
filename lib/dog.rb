27e0303c2083b98ab9371b53d3e0400648e6d7a6
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-07 02:36:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,4 +2,84 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTERGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self
+    end
+  end
+
+  def self.create(id: nil, name:, breed:)
+    new_dog = Dog.new(id: nil, name: name, breed: breed)
+    new_dog.save
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql,id)[0]
+    Dog.new(id: result[0], name: result[1], breed: result[2])
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, name, breed, id)
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    Dog.new(id: id, name: name ,breed: breed)
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ? AND breed = ? LIMIT 1
+    SQL
+    found = DB[:conn].execute(sql, name, breed).flatten
+    if found == nil
+      Dog.new(id: id, name: name, breed: breed)
+    elsif found[0] != nil
+      Dog.find_by_id(found[0])
+    else new = Dog.create(id: nil, name: name, breed: breed)
+      new
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name).flatten
+    new = Dog.new(id: result[0], name: result[1], breed: result[2])
+    new
+  end
+
 end
