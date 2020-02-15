c0b58db497a7b58a7c6a26022f90e8ce290eda19
github username <github email address>
2020-02-15 17:18:26 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,44 @@
+
+class Dog 
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(id: nil, name:, breed:) 
+        @id = id 
+        @name = name 
+        @breed = breed 
+    end 
+
+    def self.create_table 
+        sql = <<-SQL 
+            CREATE TABLE dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        SQL
+
+        DB[:conn].execute(sql)
+    end 
+
+    def self.drop_table 
+        DB[:conn].execute("DROP TABLE dogs")
+    end 
+
+    def self.new_from_db(row)
+        dog_row = Dog.new(id: row[0], name: row[1], breed: row[2])
+        dog_row 
+    end 
+
+    def save 
+        sql = <<-SQL 
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.breed) 
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        
+    end 
+
+end 
