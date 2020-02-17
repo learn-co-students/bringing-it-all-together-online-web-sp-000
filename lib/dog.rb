cf3169db7a1363713c78dd6a8a3c878da3bdb7a2
CoderJay06 <jaycru06@yahoo.com>
2020-02-16 23:43:29 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,10 +35,13 @@ class Dog
             INSERT INTO dogs (name, breed)
             VALUES (?, ?)
         SQL
-
+        
         DB[:conn].execute(sql, self.name, self.breed) 
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-        
+        Dog.new(id: @id, name: self.name, breed: self.breed)
+    end 
+
+    def self.find_by_name 
     end 
 
 end 
