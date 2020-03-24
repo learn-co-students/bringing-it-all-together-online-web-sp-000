1955d99f02a1d41955ef15d402de1568f0ed984a
Mark Harvey <harvey@DevBookMH.local>
2020-03-24 11:18:56 -0400

    pry, config, ini, cre_tab, drop

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+require 'pry'
+require_relative "../config/environment.rb"
+
+class Dog
+    attr_accessor :id, :name, :breed
+
+    def initialize (id: nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+        Create Table If Not Exists dogs (
+            id integer Primary key,
+            name Text,
+            breed Text
+        )
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = "Drop Table If Exists dogs"
+        DB[:conn].execute(sql)
+    end
+
+end
