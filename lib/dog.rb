425e50bf71a0b537b46c09d456ffa1ade69715f1
ErinRRose <your_email@example.com>
2020-05-14 09:20:17 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,6 +55,14 @@ class Dog
         SQL
 
         dog = DB[:conn].execute(sql, name, breed)
+
+        if !dog.empty?
+            dog_data = dog[0]
+            dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+        else
+            dog = self.create(name: name, breed: breed)
+        end
+        dog
     end
 
     def self.new_from_db(row)
@@ -62,6 +70,19 @@ class Dog
         name = row[1]
         breed = row[2]
         self.new(id: id, name:name, breed: breed)
+    end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+            SELECT *
+            FROM dogs
+            WHERE name = ?
+            LIMIT 1
+        SQL
+
+        DB[:conn].execute(sql, name).map do |row|
+            self.new_from_db(row)
+        end.first
     end
 
     def self.find_by_id(id)
