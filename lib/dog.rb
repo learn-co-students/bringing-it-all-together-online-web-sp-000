96b950d6ea15441c5445f8d213cb6d7d75daee03
yarvinh <yarvinhhernandez@gmail.com>
2020-06-24 16:38:31 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,58 @@
+class Dog 
+    attr_accessor :name, :breed
+    attr_reader :id
+    def initialize(id: nil , name:, breed:)
+        @id = id 
+        @name = name
+        @breed = breed
+    end
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs(
+            id INTEGER PRIMARY KEY, 
+            name TEXT, 
+            breed TEXT
+        )
+        SQL
+        DB[:conn].execute(sql)
+    end
+    def self.drop_table
+        DB[:conn].execute("DROP TABLE dogs")
+    end
+    def save
+        if self.id
+            self
+        else
+            sql = <<-SQL
+                INSERT INTO dogs(name,breed)
+                VALUES (?,?)
+            SQL
+            DB[:conn].execute(sql,self.name,self.breed)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+            self
+        end
+    end
+    def self.create(dog_hash)
+        dog = Dog.new(dog_hash)
+        dog.save
+        dog
+    end
+    def self.new_from_db(row)
+        Dog.new(id: row[0],name: row[1], breed: row[2])
+    end
+    def self.find_by_id(id)
+       dog = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id)[0]
+       self.new_from_db(dog)
+    end
+    def self.find_or_create_by(name:,breed:)     
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name,breed)
+        dog.empty? ? self.create(name: name, breed: breed) : self.find_by_id(dog[0][0])          
+    end
+    def self.find_by_name(name)
+       dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?",name)[0]
+       self.find_by_id(dog[0])
+    end
+    def update
+        DB[:conn].execute("UPDATE dogs SET name = ?, breed = ? WHERE id = ?", self.name,self.breed,self.id)
+    end
+end
--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -114,9 +114,9 @@ describe "Dog" do
       dog2 = Dog.create(name: 'teddy', breed: 'pug')
 
       dog_from_db = Dog.find_or_create_by({name: 'teddy', breed: 'cockapoo'})
-
       expect(dog_from_db.id).to eq(1)
       expect(dog_from_db.id).to eq(dog1.id)
+      
     end
     it 'when creating a new dog with the same name as persisted dogs, it returns the correct dog' do
       dog1 = Dog.create(name: 'teddy', breed: 'cockapoo')
