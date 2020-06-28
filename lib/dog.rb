945fc253a92d9119762958e779b9a18943a1aef6
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-28 04:12:03 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,8 @@ class Dog
 
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
-    result = DB[:conn].execute(sql, id)
-    Dog.new(id: result[0])
+    result = DB[:conn].execute(sql, id, name, breed)
+    Dog.new(id: result[0], name: result[1], breed: result[2])
   end
 
   def self.find_or_create_by
