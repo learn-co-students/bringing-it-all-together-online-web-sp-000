d67c95ca6f303052daa3aa9edce6f769ac2221d1
Annia Forney <anniaforney@gmail.com>
2020-09-16 19:43:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
 
   attr_reader :id
 
-  def initialize(id=nil, name:, breed:)
+  def initialize(id: nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
@@ -32,16 +32,27 @@ class Dog
   end
 
   def self.new_from_db(row)
-    id  = row[0]
+    id = row[0]
     name = row[1]
     breed = row[2]
-    self.new(id, name: name, breed: breed)
+    self.new(id: id, name: name, breed: breed)
   end
 
   def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
   end
 
-  def self.find_or_create_by
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?")
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
   end
 
   def self.find_by_name(name)
@@ -58,13 +69,18 @@ class Dog
   end
 
   def save
-    sql = <<-SQL
-     INSERT INTO dogs (name, breed)
-     VALUES (?, ?)
-   SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
 
-   DB[:conn].execute(sql, self.name, self.breed)
-   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
   end
 
   def update
