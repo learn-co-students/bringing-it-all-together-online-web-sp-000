2f6a9d34a41c4c083506b468959c02bd38d5a93d
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2020-10-05 15:14:06 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,7 +71,7 @@ class Dog
             dog_data = dog[0]
             dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
         else
-            dog = Self.create(name: name, breed: breed)
+            dog = self.create(name: name, breed: breed)
         end
         dog
     end
