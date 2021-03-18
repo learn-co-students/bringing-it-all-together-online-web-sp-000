1f3edcbfb166612005fcbde3417df403a0368e95
Chris Baum <Christopher.Baum@valpo.edu>
2021-03-17 20:48:12 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -103,7 +103,11 @@ class Dog
   end
 
   def update
-    sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?"
+    SQL
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
